39c8e8be337dfdd1aaab98eb44e74333978f803e
koksalkaradas <karadas.koksal@gmail.com>
2021-09-04 21:09:42 +0300

    fixed 404

--- src/main/java/com/sm/bd/branchdelegation/controller/contract/BranchDelegationController.java
+++ src/main/java/com/sm/bd/branchdelegation/controller/contract/BranchDelegationController.java
@@ -1,26 +1,26 @@
 package com.sm.bd.branchdelegation.controller.contract;
 
+import com.sm.bd.branchdelegation.models.BranchDelegationRequestModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping("/api/v1/bd")
 public interface BranchDelegationController {
 	
 	@PostMapping(value = "/send")
 	@ResponseStatus(HttpStatus.OK)
-	String send(@RequestBody long appId);
+	String send(@RequestBody BranchDelegationRequestModel requestModel);
 
 	@PostMapping(value = "/return")
 	@ResponseStatus(HttpStatus.OK)
-	String returnByManager(@RequestBody long appId);
+	String returnByManager(@RequestBody BranchDelegationRequestModel requestModel);
 
 	@PostMapping(value = "/approve")
 	@ResponseStatus(HttpStatus.OK)
-	String approve(@RequestBody long appId);
+	String approve(@RequestBody BranchDelegationRequestModel requestModel);
 
 	@PostMapping(value = "/reject")
 	@ResponseStatus(HttpStatus.OK)
-	String reject(@RequestBody long appId);
+	String reject(@RequestBody BranchDelegationRequestModel requestModel);
 
 }
